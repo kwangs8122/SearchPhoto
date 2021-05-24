be847a7d5ce7510c10f428d5e980b738d2b0a90c
kwangs8122 <42460592+kwangs8122@users.noreply.github.com>
2021-05-24 16:09:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/searchphoto/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.searchphoto
+
+import android.app.Dialog
+import android.graphics.Bitmap
+import android.net.http.SslError
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.view.ViewGroup
+import android.webkit.*
+import androidx.annotation.RequiresApi
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var wv = this.findViewById<WebView>(R.id.wv)
+
+        wv.apply {
+            webViewClient = WebViewClient()
+
+            webChromeClient = object : WebChromeClient() {
+                override fun onCreateWindow(
+                    view: WebView?,
+                    isDialog: Boolean,
+                    isUserGesture: Boolean,
+                    resultMsg: Message?
+                ): Boolean {
+
+                    val newWebView = WebView(this@MainActivity).apply {
+                        webViewClient = WebViewClient()
+                        settings.javaScriptEnabled = true
+                    }
+
+                    val dialog = Dialog(this@MainActivity).apply {
+                        setContentView(newWebView)
+                        window!!.attributes.width = ViewGroup.LayoutParams.MATCH_PARENT
+                        window!!.attributes.height = ViewGroup.LayoutParams.MATCH_PARENT
+                    }
+
+                    val newWebViewChromeClient = object : WebChromeClient() {
+                        override fun onCloseWindow(window: WebView?) {
+                            dialog.dismiss()
+                        }
+                    }
+
+                    (resultMsg?.obj as WebView.WebViewTransport).webView = newWebView
+                    resultMsg.sendToTarget()
+
+                    return true
+                }
+            }
+
+            settings.javaScriptEnabled = true
+            settings.setSupportMultipleWindows(true)
+            settings.javaScriptCanOpenWindowsAutomatically = true
+            settings.loadWithOverviewMode = true
+            settings.useWideViewPort = true
+            settings.setSupportZoom(true)
+
+            settings.cacheMode = WebSettings.LOAD_NO_CACHE
+            settings.domStorageEnabled = true
+            settings.displayZoomControls = true
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                settings.safeBrowsingEnabled = true
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+                settings.mediaPlaybackRequiresUserGesture = false
+            }
+
+            settings.allowContentAccess = true
+            settings.setGeolocationEnabled(true)
+            settings.allowFileAccess = true
+            settings.loadsImagesAutomatically = true
+
+            fitsSystemWindows = true
+        }
+
+        val url = "http://14.63.221.64:48084/sample/searchPhoto"
+        wv.loadUrl(url)
+    }
+
+    inner class WebViewClientClass : WebViewClient() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun shouldOverrideUrlLoading(
+            view: WebView?,
+            request: WebResourceRequest?
+        ): Boolean {
+            view?.loadUrl(request?.url.toString())
+
+            return super.shouldOverrideUrlLoading(view, request)
+        }
+
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+        }
+
+        override fun onPageCommitVisible(view: WebView?, url: String?) {
+            super.onPageCommitVisible(view, url)
+        }
+
+        override fun onReceivedSslError(
+            view: WebView?,
+            handler: SslErrorHandler?,
+            error: SslError?
+        ) {
+            super.onReceivedSslError(view, handler, error)
+        }
+    }
+}
