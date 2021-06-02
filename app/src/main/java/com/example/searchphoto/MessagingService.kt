f393652d63abe198a8be725122d968a9e5328e44
kwangs8122 <42460592+kwangs8122@users.noreply.github.com>
2021-06-02 17:30:20 +0900

    Update MessagingService.kt

--- app/src/main/java/com/example/searchphoto/MessagingService.kt
+++ app/src/main/java/com/example/searchphoto/MessagingService.kt
@@ -1,14 +1,21 @@
 package com.example.searchphoto
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 class MessagingService : FirebaseMessagingService() {
+    val TAG: String = javaClass.name
+
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
+
+        Log.d(TAG, "onMessageReceived = " + message)
     }
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
+
+        Log.d(TAG, "onNewToken = " + token)
     }
 }
