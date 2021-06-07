c52871dfc2dc93ca3173cef5a374fdee6847e5fb
kwangs8122 <42460592+kwangs8122@users.noreply.github.com>
2021-06-07 13:53:49 +0900

    s

--- app/src/main/java/com/example/searchphoto/MainActivity.kt
+++ app/src/main/java/com/example/searchphoto/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.searchphoto
 
+import android.Manifest
 import android.app.Activity
 import android.app.Dialog
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
@@ -16,6 +18,7 @@ import android.os.Bundle
 import android.os.Environment
 import android.os.Message
 import android.provider.MediaStore
+import android.provider.Settings
 import android.util.Log
 import android.view.ViewGroup
 import android.webkit.*
@@ -24,12 +27,15 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import com.example.searchphoto.common.HttpHelper
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.messaging.ktx.messaging
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.TedPermission
+import org.json.JSONObject
 import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
@@ -37,6 +43,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var requestActivity: ActivityResultLauncher<Intent>
+
+    private val TAG: String = javaClass.name
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -65,8 +73,22 @@ class MainActivity : AppCompatActivity() {
 
             // Log and toast
             val msg: String = "Token = " + token
-            Log.d("MainActivity", msg)
-            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            Log.d(TAG, msg)
+//            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+
+            var parameters: HashMap<String, Any> = HashMap<String, Any>();
+            parameters.put("USER_ID", SimpleDateFormat("yyyyMMddHHmmss").format(Date()).toString())
+            parameters.put("USER_PW", "test")
+            parameters.put("DVC_OS_TYPE", "AND")
+            parameters.put("DVC_PUSH_TOKEN", token.toString())
+            parameters.put("DVC_UUID", Settings.Secure.getString(applicationContext.contentResolver,
+                Settings.Secure.ANDROID_ID))
+
+            val response: JSONObject = HttpHelper().post("http://14.63.221.64:48084/sample/api/setToken", parameters)!!
+
+            Log.d(TAG, "result = $response")
+
+            Toast.makeText(applicationContext, response.getString("RESULT_MESSAGE"), Toast.LENGTH_SHORT).show()
         })
     }
 
