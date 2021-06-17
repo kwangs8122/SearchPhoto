4bbae33c2eff098cdedf1dfc56c1524beb595613
kwangs8122 <42460592+kwangs8122@users.noreply.github.com>
2021-06-17 16:01:16 +0900

    Update Constants.kt

--- app/src/main/java/com/example/searchphoto/common/Constants.kt
+++ app/src/main/java/com/example/searchphoto/common/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.searchphoto.common
 
 class Constants {
-    val NOTIFICATION_CHANNEL_ID: String = "NOTI_CHANNEL_SAMPEL"
+    val NOTIFICATION_CHANNEL_ID: String = "NOTI_CHANNEL_SAMPLE"
     val URL_FOR_SET_TOKEN: String = "http://14.63.221.64:48084/sample/api/setToken"
     val NAME_OF_DATABASE: String = "SampleDb.db"
     val NAME_OF_NOTIFICATION_TABLE: String = "tb_notifications"
